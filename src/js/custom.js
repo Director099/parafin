ae192924170023e0d624e414479060969203ae09
RedX <ferz099@mail.ru>
2019-01-09 00:06:51 +0500

    Почти доделал адаптив

--- src/js/custom.js
+++ src/js/custom.js
@@ -9,12 +9,53 @@ $(function () {
 $('input[type=tel]').mask("+7 (000) 000 00 00");
 
 // Плавный скол с навигации
+function scroll(element) {
+  element.click(function () {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
+    return false;
+  });
+}
 
-/* $(".scrollto > a").click(function () {
-  var elementClick = $(this).attr("href")
-  var destination = $(elementClick).offset().top;
-  jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
-  return false;
-}); */
+scroll($(".page-header__list-link"))
+// scroll($(".scroll-down"))
+// scroll($(".page-header__list-link"))
 
 // Плавный скол с навигации
+
+
+$('.sertificats__list').owlCarousel({
+  loop: false,
+  margin: 10,
+  dots: false,
+  nav: false,
+  responsive:{
+    0:{
+      items: 1
+    },
+    400:{
+      items: 2
+    },
+    768: {
+      items: 3
+    },
+    1200:{
+      items: 5
+    }
+  }
+})
+
+// $(".page-header__btn").on("click", function() {
+//   $(".page-header__btn").toggleClass("page-header__btn--active");
+// })
+
+$(".page-header__btn").on("click", function() {
+  $("body").css("overflow", "hidden");
+  $(".menu").fadeIn();
+})
+
+$(".menu__close").on("click", function() {
+  $(".menu").fadeOut();
+  $("body").css("overflow", "auto");
+})
